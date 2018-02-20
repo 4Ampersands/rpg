855b9ccc3eb12a822197c56d21368ef9a3574de3
Steele Walston <steele.walston@gmail.com>
2018-02-20 13:04:22 -0800

    bio for wizard and brute done

--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@ const brute = new Character (
     2,
     [new Item ('Heavy Armor'), new Item ('Second Weapon')],
     charAbilities.healing,
-    'Brute loves to fight. He doesn\'t really care why. He\'ll fight anyone, any time. It\'s just how brute rolls.'
+    'Zanshin hails from the distant jungle kingdom of Nyissa. Born into captivity and forced to fight in gladiatorial matches his entire life, this.troll is a versatile and proficient fighter. Despite a birth defect making his knee prone to injury (dislocated 25 times!), his combat skill - which balances both offense and defense- and his natural regenerative powers make him a force to be reckoned with. He enters the contest for a chance to buy his freedom and begin life anew..'
 );
 
 const rogue = new Character (
@@ -98,7 +98,7 @@ const rogue = new Character (
     1,
     [new Item ('Smoke Bomb'), new Item ('Backpack')],
     charAbilities.dodge,
-    'Rogue\'s parents may have been asking for trouble when they named their child "Rogue." They did their best to give her a good uppbringing. And it worked, in ways: in theory, rogue is a pascifist. But more than that, she loves gold. If she can steal it without being seen, great. If not... Hey, it\'s not her fault that acquiring gold so often requires fighting the death.'
+    'Rogue\'s parents may have been asking for trouble when they named their child "Rogue." They did their best to give her a good upbringing. And it worked, in ways: in theory, rogue is a pacifist. But more than that, she loves gold. If she can steal it without being seen, great. If not... Hey, it\'s not her fault that acquiring gold so often requires fighting to the death.'
 );
 
 const wizard = new Character (
@@ -106,7 +106,7 @@ const wizard = new Character (
     'images/wizard.png',
     1,
     0,
-    [new Item ('Healing Potion'), new Item ('Mind Control Scoll')],
+    [new Item ('Healing Potion'), new Item ('Mind Control Spell')],
     charAbilities.chaosMagic,
-    'Brute loves to fight. He doesn\'t really care why. He\'ll fight anyone, any time. It\'s just how brute rolls.'
+    'Touchstone the Tireless is a wizard of mysterious power and unknown origin. He is well versed in the schools of conjuration, telepathy, and restoration and uses these skills to bend enemies to his will. Some legends say he gained the name Tireless because of a never ending quest to rid the world of peanuts, due to a severe allergy to them.'
 );
