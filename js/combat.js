6a433137616220b5425afa6407a2159e8b6a9c71
Victor Bofill <terranvas@gmail.com>
2018-02-20 14:59:12 -0800

    Updated character selector to reflect abilities. Refined combat.js. UPdated TODO.

--- js/combat.js
+++ js/combat.js
@@ -27,8 +27,8 @@ const combat = {
 
     start: function () {
         this.load();
-        loadAbility();
-        item.equip();
+        this.loadAbility();
+        this.equipItem();
         this.renderGraphics();
         this.elements.fight.addEventListener('click', this.preFight);
         this.elements.flee.addEventListener('click', this.flee);
@@ -45,18 +45,141 @@ const combat = {
         };
 
         if (this.characterSpecs[1] === 'Heavy Armor') {
-            combat.item = heavyArmor;
-        } else if (this.characterSpecs[1] === 'Second Weapon') {
-            combat.item = secondWeapon;
-        } else if (this.characterSpecs[1] === 'Smoke Bomb') {
-            combat.item = smokeBomb;
-        } else if (this.characterSpecs[1] === 'Backpack') {
-            combat.item = backpack;
-        } else if (this.characterSpecs[1] === 'Healing Potion') {
-            combat.item = healingPotion;
-        } else if (this.characterSpecs[1] === 'Mind Control Scroll') {
-            combat.item = mindControl;
+            this.item = new Item('Heavy Armor');
         }
+        if (this.characterSpecs[1] === 'Second Weapon') {
+            this.item = new Item('Second Weapon');
+        }
+        if (this.characterSpecs[1] === 'Backpack') {
+            this.item = new Item('Backpack');
+        }
+        if (this.characterSpecs[1] === 'Smoke Bomb') {
+            this.item = new Item('Smoke Bomb');
+        }
+        if (this.characterSpecs[1] === 'Mind Control Scroll') {
+            this.item = new Item('Mind Control Scroll');
+        }
+        if (this.characterSpecs[1] === 'Healing Potion') {
+            this.item = new Item('Healing Potion');
+        }
+    },
+
+    loadAbility: function () {
+        if (combat.character.name === 'Zanshin') {
+            combat.preFight = function() {
+                if (combat.character.hp <= 10) {
+                    combat.character.hp += 1;
+                    combat.elements.announcement.textContent = 'Healed 1hp';
+                    setTimeout(function() {combat.elements.announcement.textContent = "";}, 1000);
+                }
+                combat.fight();
+            };       
+        } else if (combat.character.name === 'Rogue') {
+            combat.monsterAttack = function() {
+                const random = randomNumber(1, 100);
+                if (random < 51) {
+                    const damage = this.monster.attack - this.character.defense;
+                    if (damage > 0) {
+                        this.character.hp -= damage;
+                        this.elements.characterHP.textContent = 'HP: ' + this.character.hp;    
+                    };
+                } else if (random >= 51 ) {
+                    combat.elements.announcement.textContent = 'Dodged!';
+                    setTimeout(function() {combat.elements.announcement.textContent = "";}, 1000);
+                }
+            };
+        } else if (combat.character.name === 'Touchstone') {
+            combat.preFight = function() {
+                combat.character.attack = randomNumber(1, 4);
+                combat.character.defense = randomNumber(1, 4);
+    
+                combat.elements.announcement.textContent = 'Chaos Magic! Attack: ' + combat.character.attack + ' Defense: ' + combat.character.defense;
+                setTimeout(function() {combat.elements.announcement.textContent = "";}, 1000);
+    
+                setTimeout(combat.fight(), 1000)
+            }
+        }
+    },
+
+    equipItem: function() {
+        if (combat.item.name === 'Heavy Armor') {
+    
+            combat.character.defense = 3;
+            combat.character.portrait = 'images/brutearmor.jpeg';
+            combat.elements.characterImg.setAttribute('src', combat.character.portrait);
+    
+        } else if (combat.item.name === 'Second Weapon') {
+            combat.fight = function() {
+                while (combat.character.hp > 0 && combat.monster.hp > 0) {
+                    
+                    this.characterAttack();
+    
+                    if (this.monster.hp <=0) {
+                        // MONSTER DIES ANIMATION
+                        this.character.gold += this.monster.gold;
+                        combat.monstersDefeated++;
+                        continue;
+                    }
+                    
+                    this.monsterAttack();
+                    
+                    if (this.character.hp <= 0 ) {
+                        // PLAYER DIES ANIMATION
+    
+                        setTimeout(function() {window.location.replace('bar.html')}, 1000);
+                        continue;
+                    };                
+                };
+    
+                setTimeout(this.reset(), 1000);
+            };
+        } else if (item.name === 'Backpack') {
+            combat.fight = function() {
+                while (combat.character.hp > 0 && combat.monster.hp > 0) {
+        
+                    combat.monsterAttack();
+                    
+                    if (combat.character.hp <= 0 ) {
+                        // PLAYER DIES ANIMATION
+                        setTimeout(function() {
+                            window.location.replace('bar.html')
+                        }, 1000);
+                        continue;
+                    }
+                    
+                    combat.characterAttack();
+        
+                    if (combat.monster.hp <=0) {
+                        // MONSTER DIES ANIMATION
+                        combat.character.gold += (combat.monster.gold * 2);
+                    }
+                }
+                
+                setTimeout(combat.reset(), 1000);
+        
+            };
+        } else if (combat.item.name === 'Smoke Bomb') {
+            item.use = function() {
+                combat.character.gold += (combat.monster.gold * 2);
+                combat.elements.announcement.textContent = 'Smoke Bomb used!';
+                combat.monstersDefeated++;
+    
+                setTimeout(combat.reset(), 1000);
+            }
+        } else if (combat.item.name === 'Healing Potion') {
+            item.use = function() {
+                if (combat.item.used === false) {
+                    combat.item.used = true;
+                    
+                    combat.character.hp = 10;
+    
+                    combat.elements.announcement.textContent = 'Healing Potion used. Health fully restored!';
+                    
+                    combat.elements.characterHP.textContent = 'HP: ' + combat.character.hp;                
+                };
+            };
+        };
+    
     },
 
     renderGraphics: function () {
@@ -148,137 +271,6 @@ const combat = {
         this.createMonster();
         this.renderGraphics();
     }
-};
-
-// CHARACTER ABILITIES
-const loadAbility = function () {
-    if (combat.character.name === 'Zanshin') {
-        combat.preFight = function() {
-            if (combat.character.hp <= 10) {
-                combat.character.hp += 1;
-                combat.elements.announcement.textContent = 'Healed 1hp';
-                setTimeout(function() {combat.elements.announcement.textContent = "";}, 1000);
-            }
-            combat.fight();
-        };       
-    } else if (combat.character.name === 'Rogue') {
-        combat.monsterAttack = function() {
-            const random = randomNumber(1, 100);
-            if (random < 51) {
-                const damage = this.monster.attack - this.character.defense;
-                if (damage > 0) {
-                    this.character.hp -= damage;
-                    this.elements.characterHP.textContent = 'HP: ' + this.character.hp;    
-                };
-            } else if (random >= 51 ) {
-                combat.elements.announcement.textContent = 'Dodged!';
-                setTimeout(function() {combat.elements.announcement.textContent = "";}, 1000);
-            }
-        };
-    } else if (combat.character.name === 'Touchstone') {
-        combat.preFight = function() {
-            combat.character.attack = randomNumber(1, 10);
-            combat.character.defense = randomNumber(1, 10);
-
-            combat.elements.announcement.textContent = 'Chaos Magic! Attack: ' + combat.character.attack + ' Defense: ' + combat.character.defense;
-            setTimeout(function() {combat.elements.announcement.textContent = "";}, 1000);
-
-            setTimeout(combat.fight(), 1000)
-        }
-    }
-
-};
-
-// ITEMS
-
-const heavyArmor = new Item('Heavy Armor');
-const secondWeapon = new Item('Second Weapon');
-const backpack = new Item('Backpack');
-const smokeBomb = new Item('Smoke Bomb');
-const mindControl = new Item('Mind Control Scroll');
-const healingPotion = new Item('Healing Potion');
-
-item.equip = function() {
-    if (combat.item.name === 'Heavy Armor') {
-
-        combat.character.defense = 3;
-        combat.character.portrait = 'images/brutearmor.jpeg';
-        combat.elements.characterImg.setAttribute('src', combat.character.portrait);
-
-    } else if (combat.item.name === 'Second Weapon') {
-        combat.fight = function() {
-            while (combat.character.hp > 0 && combat.monster.hp > 0) {
-                
-                this.characterAttack();
-
-                if (this.monster.hp <=0) {
-                    // MONSTER DIES ANIMATION
-                    this.character.gold += this.monster.gold;
-                    combat.monstersDefeated++;
-                    continue;
-                }
-                
-                this.monsterAttack();
-                
-                if (this.character.hp <= 0 ) {
-                    // PLAYER DIES ANIMATION
-
-                    setTimeout(function() {window.location.replace('bar.html')}, 1000);
-                    continue;
-                };                
-            };
-
-            setTimeout(this.reset(), 1000);
-        };
-    } else if (item.name === 'Backpack') {
-        combat.fight = function() {
-            while (combat.character.hp > 0 && combat.monster.hp > 0) {
-    
-                combat.monsterAttack();
-                
-                if (combat.character.hp <= 0 ) {
-                    // PLAYER DIES ANIMATION
-                    setTimeout(function() {
-                        window.location.replace('bar.html')
-                    }, 1000);
-                    continue;
-                }
-                
-                combat.characterAttack();
-    
-                if (combat.monster.hp <=0) {
-                    // MONSTER DIES ANIMATION
-                    combat.character.gold += (combat.monster.gold * 2);
-                }
-            }
-            
-            setTimeout(combat.reset(), 1000);
-    
-        };
-    } else if (combat.item.name === 'Smoke Bomb') {
-        item.use = function() {
-            combat.character.gold += (combat.monster.gold * 2);
-            combat.elements.announcement.textContent = 'Smoke Bomb used!';
-            combat.monstersDefeated++;
-
-            setTimeout(combat.reset(), 1000);
-        }
-    } else if (combat.item.name === 'Healing Potion') {
-        item.use = function() {
-            if (combat.item.used === false) {
-                combat.item.used = true;
-                
-                combat.character.hp = 10;
-
-                combat.elements.announcement.textContent = 'Healing Potion used!';
-                
-                combat.elements.characterHP.textContent = combat.character.hp;                
-            };
-
-            setTimeout(combat.reset(), 1000);
-        }
-    };
-
 };
 
 combat.start();
