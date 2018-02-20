b008f001515613331032d1005cdc1added12dc33
Victor Bofill <terranvas@gmail.com>
2018-02-19 16:58:37 -0800

    Basic game structure works

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const randomNumber = function(min, max) {
 // CHARACTER CONSTRUCTOR
 // name should be their name.
 // portrait should be a path to a character portrait as a string
-// inventory should be an array containing an inventory Item
+// inventory should be a Item
 // attack and defense should be integers
 // ability should be a method of the Character.prototype object
 // bio should be a short bio of the character as a string
@@ -35,7 +35,7 @@ Character.prototype.useItem = function () {
 
 Character.prototype.run = function () {
     // end the game and apply sore to high score table
-};
+}
 
 const charAbilities = {
     healing: function() {
@@ -51,59 +51,24 @@ const charAbilities = {
     }
 };
 
-// ITEMS
-Item = function (name) {
-    this.name = name;
-
-    this.equipped = false;
-    this.used = false;
-};
-
 // MONSTERS
 SmallMonster = function () {
     this.portrait = 'images/smallmonster.png';
     this.hp = 1;
-    this.attack = randomNumber(1,2);
+    this.gold = randomNumber(3,5);
+    this.attack = randomNumber(2,3);
 };
 
 MediumMonster = function () {
     this.portrait = 'images/mediummonster.jpg'
     this.hp = 2;
-    this.attack = randomNumber(2,3);
+    this.gold = randomNumber(5,8);
+    this.attack = randomNumber(3,4);
 };
 
 LargeMonster = function () {
     this.hp = 3;
     this.portrait = 'images/largemonster.jpg'
-    this.attack = randomNumber(3,4);
+    this.gold = randomNumber(8,12);
+    this.attack = randomNumber(5,6);
 };
-
-const brute = new Character (
-    'Brute',
-    'images/brute.png',
-    5,
-    5,
-    [new Item ('Heavy Armor'), new Item ('Second Weapon')],
-    charAbilities.healing,
-    'Brute loves to fight. He doesn\'t really care why. He\'ll fight anyone, any time. It\'s just how brute rolls.'
-);
-
-const rogue = new Character (
-    'Rogue',
-    'images/rogue.png',
-    3,
-    3,
-    [new Item ('Smoke Bomb'), new Item ('Backpack')],
-    charAbilities.dodge,
-    'Brute loves to fight. He doesn\'t really care why. He\'ll fight anyone, any time. It\'s just how brute rolls.'
-);
-
-const wizard = new Character (
-    'Wizard',
-    'images/wizard.png',
-    1,
-    1,
-    [new Item ('Healing Potion'), new Item ('Mind Control Scoll')],
-    charAbilities.chaosMagic,
-    'Brute loves to fight. He doesn\'t really care why. He\'ll fight anyone, any time. It\'s just how brute rolls.'
-);
--- js/combat.js
+++ js/combat.js
@@ -1,12 +1,54 @@
 'use strict';
 
-// placeholder characters before localStorage is set up
+// ITEMS
+Item = function (name) {
+    this.name = name;
+
+    this.equipped = false;
+    this.used = false;
+};
+
+// CHARACTERS
+// ONCE LOCAL STORAGE IS UP, ONLY MAKE ONE CHARACTER BASED ON LOCAL STORAGE
+const brute = new Character (
+    'Brute',
+    'images/brute.png',
+    5,
+    2,
+    [new Item ('Heavy Armor'), new Item ('Second Weapon')],
+    charAbilities.healing,
+    'Brute loves to fight. He doesn\'t really care why. He\'ll fight anyone, any time. It\'s just how brute rolls.'
+);
+
+const rogue = new Character (
+    'Rogue',
+    'images/rogue.png',
+    3,
+    1,
+    [new Item ('Smoke Bomb'), new Item ('Backpack')],
+    charAbilities.dodge,
+    'Brute loves to fight. He doesn\'t really care why. He\'ll fight anyone, any time. It\'s just how brute rolls.'
+);
+
+const wizard = new Character (
+    'Wizard',
+    'images/wizard.png',
+    1,
+    0,
+    [new Item ('Healing Potion'), new Item ('Mind Control Scoll')],
+    charAbilities.chaosMagic,
+    'Brute loves to fight. He doesn\'t really care why. He\'ll fight anyone, any time. It\'s just how brute rolls.'
+);
+
+// PLACEHOLDER UNTIL LOCAL STORAGE IS UP
 let character = brute;
 
 const elements = {
     mainDungeon: document.getElementById('main-dungeon'),
     characterDiv: document.getElementById('character-div'),
     characterImg: document.getElementById('character-img'),
+    characterHP: document.getElementById('character-hp'),
+
     monsterDiv: document.getElementById('monster-div'),
     monsterImg: document.getElementById('monster-img'),
 
@@ -33,30 +75,60 @@ const createMonster = function () {
 // load localStorage data
 
 // load all visual elements
+// STILL NEEDS TO UTILIZE LOCALSTORAGE DATA
 const renderGraphics = function () {
     elements.characterImg.setAttribute('src', character.portrait);
+    elements.characterHP.textContent = character.hp;
+
     elements.monsterImg.setAttribute('src', monster.portrait);
-    // elements.item.textContent(character.item);
+    elements.item.textContent = (character.inventory[0].name);
 }
 
 // add event listeners
 elements.fight.addEventListener('click', function() {
+    while (character.hp > 0 && monster.hp > 0) {
+        const damage = monster.attack - character.defense;
+        if (damage > 0) {
+            character.hp -= damage;
+        }
+        console.log('Attack: ' + monster.attack);
+        console.log('Damage: ' + damage);
+        elements.characterHP.textContent = character.hp;
+        if (character.hp <= 0 ) {
+            // PLAYER DIES ANIMATION
+            console.log('You died.');
+            setTimeout(function() {
+                window.location.replace('bar.html')
+            }, 1000);
+            continue;
+        }
+        monster.hp -= character.attack;
+        console.log('Monster: ' + monster.hp);
 
-
+        if (monster.hp <=0) {
+            character.gold += monster.gold;
+            console.log('Gold: ' + character.gold);
+        }
+    }
+    setTimeout(function() {
+        reset()}, 1000);
 });
 
 elements.flee.addEventListener('click', function() {
-// character turns and runs
-// screen fades to black
+// CHARACTER TURNS AND RUNS, SCREEN TO BLACK ANIMATION
 
     setTimeout(function() {
-        window.location.replace('bar.html')
+        window.location.replace('leaderboard.html')
     }, 1000);
 });
 
 elements.item.addEventListener('click', function() {
-
 });
+
+const reset = function() {
+    createMonster();
+    renderGraphics();
+}
 
 createMonster();
 renderGraphics();
